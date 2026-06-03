bb92674041bafc5773ce5fce22da94dbab735d6d
ashishchoudhary999 <ashishchoudharyofficial0@gmail.com>
2026-06-03 16:17:55 +0530

    feat: add database config

--- app/database.py
+++ app/database.py
@@ -6,3 +6,23 @@ from sqlalchemy.orm import sessionmaker, declarative_base
 load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
+
+if DATABASE_URL and DATABASE_URL.startswitch("postgres://"):
+    DATABASE_URL = DATABASE_URL.replace("postgres://", "postgresql://", 1)
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_pings=True,
+    pool_recycle=300
+)
+
+sessionLocal = sessionmaker(bind=engine, autoflush=False, autocommit=False)
+
+Base = declarative_base()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
